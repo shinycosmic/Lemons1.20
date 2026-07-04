a4e0ff3dbf0353732e6d8ca8159216533a037cbb
shcosmic <razorrotom@gmail.com>
2026-07-04 12:49:09 -0400

    Holonet Tooltip

--- src/main/java/net/lemon/animalia/item/HolonetItem.java
+++ src/main/java/net/lemon/animalia/item/HolonetItem.java
@@ -1,12 +1,18 @@
 package net.lemon.animalia.item;
 
 import net.lemon.animalia.client.screens.HolonetScreenOpener;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class HolonetItem extends Item {
 
@@ -21,4 +27,11 @@ public class HolonetItem extends Item {
         }
         return InteractionResultHolder.sidedSuccess(player.getItemInHand(hand), level.isClientSide());
     }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltipComp, TooltipFlag isAdvanced) {
+        super.appendHoverText(stack, level, tooltipComp, isAdvanced);
+        tooltipComp.add(Component.empty()
+                .append(Component.translatable("tooltip.animalia.holonet.action").withStyle(ChatFormatting.BOLD, ChatFormatting.BLUE))
+                .append(Component.translatable("tooltip.animalia.holonet.desc").withStyle(ChatFormatting.GRAY)));    }
 }
