fe210fb9a2b8de7b59aa0d51b50105198ae76963
shcosmic <razorrotom@gmail.com>
2026-07-11 21:27:48 -0400

    fix lighting function

--- src/main/java/net/lemon/animalia/util/AnimaliaFunctionUtil.java
+++ src/main/java/net/lemon/animalia/util/AnimaliaFunctionUtil.java
@@ -1,5 +1,15 @@
 package net.lemon.animalia.util;
 
+import com.mojang.blaze3d.platform.Lighting;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
+import net.minecraft.world.entity.LivingEntity;
+import org.joml.Matrix4f;
+import org.joml.Quaternionf;
+
 public class AnimaliaFunctionUtil {
     private static final float CM_PER_PIXEL = 6.25f; // 1 block = 16px = 100cm
 
@@ -13,5 +23,28 @@ public class AnimaliaFunctionUtil {
      */
     public static float getScaleForSize(float modelPixelLength, float desiredCm) {
         return desiredCm / (modelPixelLength * CM_PER_PIXEL);
+    }
+
+    public static void renderEntityFlat(GuiGraphics graphics, int x, int y, int scale, Quaternionf rotation, LivingEntity entity) {
+        graphics.pose().pushPose();
+        graphics.pose().translate(x, y, 50.0);
+        graphics.pose().mulPoseMatrix(new Matrix4f().scaling(scale, scale, -scale));
+        graphics.pose().mulPose(rotation);
+
+        Lighting.setupForFlatItems();
+
+        EntityRenderDispatcher dispatcher = Minecraft.getInstance().getEntityRenderDispatcher();
+        dispatcher.setRenderShadow(false);
+        dispatcher.overrideCameraOrientation(new Quaternionf());
+
+        MultiBufferSource.BufferSource bufferSource = Minecraft.getInstance().renderBuffers().bufferSource();
+        RenderSystem.runAsFancy(() -> {
+            dispatcher.render(entity, 0, 0, 0, 0.0F, 1.0F, graphics.pose(), bufferSource, 15728880);
+        });
+        bufferSource.endBatch();
+
+        dispatcher.setRenderShadow(true);
+        graphics.pose().popPose();
+        Lighting.setupFor3DItems();
     }
 }
