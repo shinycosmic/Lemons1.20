bd8cf820ab74e45a79aaa1279460c9ce77866380
shcosmic <razorrotom@gmail.com>
2026-07-13 14:36:51 -0400

    Keybind to open holonet as long as it is in your inventory

---
+++ src/main/java/net/lemon/animalia/registry/ModKeyBindings.java
@@ -0,0 +1,24 @@
+package net.lemon.animalia.registry;
+
+import net.lemon.animalia.Animalia;
+import net.minecraft.client.KeyMapping;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.lwjgl.glfw.GLFW;
+
+@Mod.EventBusSubscriber(modid = Animalia.MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ModKeyBindings {
+
+    public static final KeyMapping OPEN_HOLONET = new KeyMapping(
+            "key.animalia.open_holonet",       // translation key (shown in controls menu)
+            GLFW.GLFW_KEY_G,                   // default key
+            "key.categories.animalia"          // category heading in controls menu
+    );
+
+    @SubscribeEvent
+    public static void onRegisterKeyMappings(RegisterKeyMappingsEvent event) {
+        event.register(OPEN_HOLONET);
+    }
+}
--- src/main/java/net/lemon/animalia/registry/events/ModEventBusClientEvents.java
+++ src/main/java/net/lemon/animalia/registry/events/ModEventBusClientEvents.java
@@ -2,12 +2,17 @@ package net.lemon.animalia.registry.events;
 
 import net.lemon.animalia.Animalia;
 import net.lemon.animalia.client.screens.*;
+import net.lemon.animalia.registry.ModItems;
+import net.lemon.animalia.registry.ModKeyBindings;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -23,6 +28,26 @@ public class ModEventBusClientEvents {
                 || screen instanceof OrderGridScreen
                 || screen instanceof CreatureDetailScreen) {
                 event.setCanceled(true);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.phase != TickEvent.Phase.END) return;
+
+        Minecraft mc = Minecraft.getInstance();
+        if (mc.player == null || mc.screen != null) return;
+
+        if (ModKeyBindings.OPEN_HOLONET.consumeClick()) {
+            Player player = mc.player;
+            Inventory inventory = player.getInventory();
+
+            for (int i = 0; i < inventory.getContainerSize(); i++) {
+                if (inventory.getItem(i).is(ModItems.HOLONET.get())) {
+                    HolonetScreenOpener.open();
+                    return;
+                }
             }
         }
     }
