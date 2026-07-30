a00fb9b094a738f7e1a4c453d807668c587ac0ce
shcosmic <razorrotom@gmail.com>
2026-07-29 20:57:02 -0400

    mobs auto register

--- src/main/java/net/lemon/animalia/registry/creativetabs/AnimaliaMobsTab.java
+++ src/main/java/net/lemon/animalia/registry/creativetabs/AnimaliaMobsTab.java
@@ -3,37 +3,16 @@ package net.lemon.animalia.registry.creativetabs;
 import net.lemon.animalia.registry.ModItems;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SpawnEggItem;
 import net.minecraftforge.registries.RegistryObject;
 
-import java.util.List;
-
 public class AnimaliaMobsTab {
-    public static final List<RegistryObject<? extends Item>> SPAWN_EGGS = List.of(
-            //Spawn Eggs
-            ModItems.CHILEANSEABASS_SPAWN_EGG,
-            ModItems.ELEGINOPS_MACLOVINUS_SPAWN_EGG,
-            ModItems.PSEUDAPHRITIS_URVILLII_SPAWN_EGG,
-            ModItems.PERCOPHIS_BRASILIENSIS_SPAWN_EGG,
-            ModItems.POGONOPHRYNE_MARMORATA_SPAWN_EGG,
-            ModItems.CYGNODRACO_MAWSONI_SPAWN_EGG,
-            ModItems.CHAENOCEPHALUS_ACERATUS_SPAWN_EGG,
-            ModItems.SYNBRANCHUS_MARMORATUS_SPAWN_EGG,
-            ModItems.CHAUDHURIA_CAUDATA_SPAWN_EGG,
-            ModItems.MASTACEMBELUS_ARMATUS_SPAWN_EGG,
-            ModItems.MASTACEMBELUS_ERYTHROTAENIA_SPAWN_EGG,
-            ModItems.MASTACEMBELUS_BRICHARDI_SPAWN_EGG,
-            ModItems.MACROGNATHUS_SIAMENSIS_SPAWN_EGG,
-            ModItems.SINOBDELLA_SINENSIS_SPAWN_EGG,
-            ModItems.RAKTHAMICHTHYS_INDICUS_SPAWN_EGG,
-            ModItems.NEMATISTIUS_PECTORALIS_SPAWN_EGG,
-            ModItems.TOXOTES_CHATAREUS_SPAWN_EGG,
-            ModItems.BETTA_SPLENDENS_SPAWN_EGG,
-            ModItems.SCATOPHAGUS_ARGUS_SPAWN_EGG
-
-
-    );
 
     public static void displayItems(CreativeModeTab.Output output) {
-        SPAWN_EGGS.forEach(item -> output.accept(item.get()));
+        for (RegistryObject<Item> entry : ModItems.ITEMS.getEntries()) {
+            if (entry.get() instanceof SpawnEggItem egg) {
+                output.accept(egg);
+            }
+        }
     }
 }
