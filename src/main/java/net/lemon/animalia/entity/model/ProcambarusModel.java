88c3a22b14bab2e814587858c289e73b96634b84
shinycosmic <razorrotom@gmail.com>
2026-07-29 23:50:00 -0400

    Procambarus fixes

--- src/main/java/net/lemon/animalia/entity/model/ProcambarusModel.java
+++ src/main/java/net/lemon/animalia/entity/model/ProcambarusModel.java
@@ -1,15 +1,10 @@
-package net.lemon.animalia.entity.model;//package net.lemon.animalia.entity.model;
+package net.lemon.animalia.entity.model;
 
 import net.lemon.animalia.Animalia;
 import net.lemon.animalia.entity.custom.CrayfishEntity;
-import net.lemon.animalia.entity.custom.RegSchoolingEntity;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.registries.ForgeRegistries;
-import software.bernie.geckolib.constant.DataTickets;
-import software.bernie.geckolib.core.animatable.model.CoreGeoBone;
-import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.model.GeoModel;
-import software.bernie.geckolib.model.data.EntityModelData;
 
 public class ProcambarusModel extends GeoModel<CrayfishEntity> {
     public ResourceLocation getModelResource(CrayfishEntity object) {
