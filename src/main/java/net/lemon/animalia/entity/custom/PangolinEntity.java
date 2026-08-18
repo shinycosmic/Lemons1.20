7a9d1e695220edcf4e35089cd13233391a473509
shcosmic <razorrotom@gmail.com>
2026-08-18 14:39:21 -0400

    Mid guard attack for pangolin

--- src/main/java/net/lemon/animalia/entity/custom/PangolinEntity.java
+++ src/main/java/net/lemon/animalia/entity/custom/PangolinEntity.java
@@ -19,14 +19,12 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.MobSpawnType;
-import net.minecraft.world.entity.SpawnGroupData;
+import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.BreedGoal;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
@@ -37,6 +35,7 @@ import software.bernie.geckolib.core.animatable.GeoAnimatable;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.*;
+import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.core.object.PlayState;
 
 public class PangolinEntity extends AnimaliaLandBase implements GeoEntity, Scannable, ICanGuard {
@@ -45,6 +44,8 @@ public class PangolinEntity extends AnimaliaLandBase implements GeoEntity, Scann
     private final int SMUTSIA_GIGANTEA_PIXEL = 39;
 
     private int wantsToGuardUntil;
+    private int attackCooldown;
+
 
     public PangolinEntity(EntityType<? extends Animal> entityType, Level level) {
         super(entityType, level);
@@ -52,7 +53,7 @@ public class PangolinEntity extends AnimaliaLandBase implements GeoEntity, Scann
 
     @Override
     protected void registerGoals() {
-        this.goalSelector.addGoal(1, new GuardGoal(this, 6D));
+        this.goalSelector.addGoal(1, new GuardGoal(this, 6D, entity -> !(entity instanceof PangolinEntity) && !(entity instanceof Player player && player.isCreative())));
         super.registerGoals();
     }
 
@@ -143,12 +144,12 @@ public class PangolinEntity extends AnimaliaLandBase implements GeoEntity, Scann
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
-        controllers.add(new AnimationController<>(this, "controller", 10, this::predicate));
+        controllers.add(new AnimationController<>(this, "controller", 10, this::predicate).triggerableAnim("attack", RawAnimation.begin().then("attack", Animation.LoopType.PLAY_ONCE)));
         controllers.add(new AnimationController<>(this, "eat_controller", 0, this::eatPredicate));
     }
 
     private <T extends GeoAnimatable> PlayState predicate(AnimationState<T> animationState) {
-        if (this.isGrazing() && !this.isBaby()) { //TODO handle grazing only at termite mound block
+        if (this.isGrazing() && !this.isBaby()) {
             animationState.getController().setAnimation(RawAnimation.begin().then("graze", Animation.LoopType.LOOP));
             return PlayState.CONTINUE;
         }
@@ -156,7 +157,7 @@ public class PangolinEntity extends AnimaliaLandBase implements GeoEntity, Scann
 
         if (!isBaby()) {
             int phase = this.getGuardPhase();
-            //TODO need to add handling for attack during guard, and 1 random idle, sleeping(looped without special enter and exit anims) and dream (idle during sleep only)
+            //TODO need to add 1 random idle, sleeping(looped without special enter and exit anims) and dream (idle during sleep only)
             switch (phase) {
                 case GUARD_PHASE_ENTERING:
                     animationState.getController().setAnimation(RawAnimation.begin().then("toGuard", Animation.LoopType.HOLD_ON_LAST_FRAME));
@@ -243,6 +244,26 @@ public class PangolinEntity extends AnimaliaLandBase implements GeoEntity, Scann
             this.guardWindow(this.getGuardReAttackWindow());
         }
         return super.hurt(source, amount * this.getGuardDamageMultiplier(source));
+    }
+
+    @Override
+    public void onGuardTick(LivingEntity threat) {
+        if (this.attackCooldown > 0) {
+            return;
+        }
+        if (this.getBoundingBox().inflate(0.3D).intersects(threat.getBoundingBox())) {
+            this.doHurtTarget(threat);
+            this.triggerAnim("controller", "attack");
+            this.attackCooldown = 15;
+        }
+    }
+
+    @Override
+    public void aiStep() {
+        super.aiStep();
+        if (!this.level().isClientSide && this.attackCooldown > 0) {
+            this.attackCooldown--;
+        }
     }
 
     @Override
