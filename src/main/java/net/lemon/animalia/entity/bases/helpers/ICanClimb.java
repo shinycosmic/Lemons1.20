191361d8aba634ead9a09e89b9e9302b0585cf22
shcosmic <razorrotom@gmail.com>
2026-09-09 15:39:14 -0400

    Crossarchus+Climber prep

---
+++ src/main/java/net/lemon/animalia/entity/bases/helpers/ICanClimb.java
@@ -0,0 +1,174 @@
+package net.lemon.animalia.entity.bases.helpers;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MoverType;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+
+public interface ICanClimb {
+
+    Direction[] CLIMB_FACES = new Direction[]{Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST, Direction.UP};
+
+    static boolean onSolid(Mob mob) {
+        return mob.onGround() || (mob instanceof ICanClimb climber && climber.isAttached());
+    }
+
+    byte getClimbData();
+
+    void setClimbData(byte data);
+
+    boolean wantsToClimb();
+
+    void setWantsToClimb(boolean wantsToClimb);
+
+    default boolean canClimb() {
+        return false;
+    }
+
+    default boolean canClimbUp() {
+        return true;
+    }
+
+    default boolean canClimbDown() {
+        return true;
+    }
+
+    default boolean canClimbCeilings() {
+        return false;
+    }
+
+    default float climbUpMalus() {
+        return 0.0F;
+    }
+
+    default float climbDownMalus() {
+        return 0.0F;
+    }
+
+    default boolean isClimbableBlock(BlockState state) {
+        return true;
+    }
+
+    default double getClimbSpeed() {
+        return 1.0D;
+    }
+
+    /**
+     * basically, this method controls if during pathfinding, the mob should treat walls as valid passes
+     */
+    default boolean canPathfindWithWalls() {
+        return this.canClimb() && (this.wantsToClimb() || this.isAttached());
+    }
+
+    default Direction getClimbFace() {
+        return Direction.from3DDataValue(this.getClimbData() & 7);
+    }
+
+    default void setClimbFace(Direction face) {
+        this.setClimbData((byte) ((this.getClimbData() & ~7) | face.get3DDataValue()));
+    }
+
+    default boolean isAttached() {
+        return this.getClimbFace() != Direction.DOWN;
+    }
+
+    default void detach() {
+        this.setClimbFace(Direction.DOWN);
+        ((Mob) this).setYya(0.0F);
+    }
+
+    default boolean faceAllowed(Direction face) {
+        return face != Direction.UP || this.canClimbCeilings();
+    }
+
+    default boolean canAttachTo(BlockGetter level, BlockPos pos) {
+        BlockState state = level.getBlockState(pos);
+        return !state.getCollisionShape(level, pos).isEmpty() && this.isClimbableBlock(state);
+    }
+
+    default boolean hasClimbableFace(BlockGetter level, BlockPos pos) {
+        for (Direction face : CLIMB_FACES) {
+            if (this.faceAllowed(face) && this.canAttachTo(level, pos.relative(face))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    default boolean isValidAttachment(Direction face) {
+        PathfinderMob mob = (PathfinderMob) this;
+        BlockPos support = mob.blockPosition().relative(face);
+        return mob.level().isLoaded(support) && this.canAttachTo(mob.level(), support);
+    }
+
+    default boolean attach() {
+        if (!this.canClimb()) {
+            return false;
+        }
+        this.selectAttachmentFace();
+        return this.isAttached();
+    }
+
+    default void selectAttachmentFace() {
+        PathfinderMob mob = (PathfinderMob) this;
+        Direction current = this.getClimbFace();
+        Direction best = null;
+        double bestDist = Double.MAX_VALUE;
+        if (current != Direction.DOWN && this.faceAllowed(current) && this.isValidAttachment(current)) {
+            best = current;
+            bestDist = this.faceDistSqr(mob, current);
+        }
+        for (Direction face : CLIMB_FACES) {
+            if (face == current || !this.faceAllowed(face) || !this.isValidAttachment(face)) {
+                continue;
+            }
+            double dist = this.faceDistSqr(mob, face);
+            if (dist < bestDist) {
+                best = face;
+                bestDist = dist;
+            }
+        }
+        if (best == null) {
+            this.detach();
+            return;
+        }
+        this.setClimbFace(best);
+    }
+
+    default double faceDistSqr(PathfinderMob mob, Direction face) {
+        BlockPos support = mob.blockPosition().relative(face);
+        return mob.distanceToSqr(support.getX() + 0.5D, support.getY() + 0.5D, support.getZ() + 0.5D);
+    }
+
+    default void tickClimb() {
+        if (!this.isAttached()) {
+            return;
+        }
+        PathfinderMob mob = (PathfinderMob) this;
+        if (!this.canClimb() || mob.isPassenger() || mob.isLeashed()
+                || (!this.wantsToClimb() && mob.onGround())) {
+            this.detach();
+            return;
+        }
+        if (!this.faceAllowed(this.getClimbFace()) || !this.isValidAttachment(this.getClimbFace())) {
+            this.selectAttachmentFace();
+        }
+    }
+
+    default void climbTravel(Vec3 travelVector) {
+        PathfinderMob mob = (PathfinderMob) this;
+        Direction face = this.getClimbFace();
+        Vec3 drive = travelVector.multiply(1 - Math.abs(face.getStepX()), 1 - Math.abs(face.getStepY()), 1 - Math.abs(face.getStepZ()));
+        mob.addDeltaMovement(drive.scale(this.getClimbSpeed() * 0.005D));
+        if (!mob.horizontalCollision && !mob.verticalCollision) {
+            mob.addDeltaMovement(new Vec3(face.getStepX() * 0.01D, face.getStepY() * 0.01D, face.getStepZ() * 0.01D));
+        }
+        mob.move(MoverType.SELF, mob.getDeltaMovement());
+        mob.setDeltaMovement(mob.getDeltaMovement().scale(0.9D));
+        mob.resetFallDistance();
+    }
+}
---
+++ src/main/java/net/lemon/animalia/entity/custom/CrossarchusEntity.java
@@ -0,0 +1,108 @@
+package net.lemon.animalia.entity.custom;
+
+import net.lemon.animalia.entity.bases.AnimaliaLandBase;
+import net.lemon.animalia.entity.bases.helpers.ActivityTime;
+import net.lemon.animalia.registry.ModEntities;
+import net.lemon.animalia.registry.ModItems;
+import net.lemon.animalia.registry.ModTags;
+import net.lemon.animalia.util.AnimaliaFunctionUtil;
+import net.lemon.animalia.util.HolonetEntities;
+import net.lemon.animalia.util.Scannable;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.SpawnGroupData;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
+import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.animatable.GeoEntity;
+import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.AnimatableManager;
+
+public class CrossarchusEntity extends AnimaliaLandBase implements GeoEntity, Scannable {
+    private final AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
+
+    public CrossarchusEntity(EntityType<? extends Animal> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Override
+    public Item getBreedingItem() {
+        return ModItems.TERMITE.get();
+    }
+
+    @Override
+    public TagKey<Item> getFoodTag() {
+        return ModTags.Items.INVERTEBRATE;
+    }
+
+    @Override
+    public ActivityTime activityTime() {
+        return ActivityTime.DIURNAL;
+    }
+
+    @Override
+    public AppName getApp() {
+        return AppName.FIELD;
+    }
+
+    @Override
+    public Component getTrivia() {
+        return Component.translatable("trivia.animalia.crossarchus");
+    }
+
+    @Override
+    public Component getFamily() {
+        return Component.translatable("family.animalia.herpestidae");
+    }
+
+    @Override
+    public Component getOrder() {
+        return Component.translatable("order.animalia.carnivora");
+    }
+
+    @Override
+    public int getScaleforGUI() {
+//        if (this.getType() == ModEntities.SMUTSIA_GIGANTEA.get()) {
+//            return 22;
+//        }
+        return Scannable.super.getScaleforGUI();
+    }
+
+    public static void registerHolonet(){
+//        HolonetEntities.register(ModEntities.SMUTSIA_GIGANTEA, AppName.FIELD, "Pholidota");
+    }
+
+    @Override
+    public float genVarSizeMultiplier() {
+//        if (this.getType() == ModEntities.SMUTSIA_GIGANTEA.get()) {
+//            return AnimaliaFunctionUtil.getScaleForSize(39, 137);
+//        }
+        return 1;
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
+
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return cache;
+    }
+
+    @Override
+    public @Nullable SpawnGroupData finalizeSpawn(ServerLevelAccessor level, DifficultyInstance difficulty, MobSpawnType reason, @Nullable SpawnGroupData spawnData, @Nullable CompoundTag dataTag) {
+        if (dataTag == null) {
+            this.setVarColor(1);
+            this.setVarSizeMultiplier(this.genVarSizeMultiplier());
+        }
+        return super.finalizeSpawn(level, difficulty, reason, spawnData, dataTag);
+    }
+}
